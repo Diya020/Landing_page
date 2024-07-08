84aea156d3be32b0fc8ff35fcc202aac02124e09
Diya020 <sutrishnamondal870@gmail.com>
2024-07-08 20:54:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function myButton() {
+  let email=prompt("Please enter your Email:");
+  if(email==null||email==""||email!="@")
+  {
+    let txt=alert("Please enter your Email.");
+  }else{
+    txt=alert("THANK YOU!");
+  }
+  
+}
